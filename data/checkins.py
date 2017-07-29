0ca1f2bfb509feff76aca4f7bd3ea31acb51edcb
Stanley Lee <stanleykylee@users.noreply.github.com>
2017-07-29 03:00:28 -0700

    working line chart

--- data/checkins.py
+++ data/checkins.py
@@ -1,19 +1,20 @@
 # -*- coding: utf-8 -*-
 import csv
 import pandas as pd
+from datetime import datetime
 
 # create empty dictionary
 d = {}
 
 def daytonumber(x):
     return {
-        'Mon': '1',
-        'Tue': '2',
-        'Wed': '3',
-        'Thu': '4',
-        'Fri': '5',
-        'Sat': '6',
-        'Sun': '7',
+        'Mon': 1,
+        'Tue': 2,
+        'Wed': 3,
+        'Thu': 4,
+        'Fri': 5,
+        'Sat': 6,
+        'Sun': 7,
     }[x]
 
 def addCategory(checkin, category, checkins):
@@ -24,9 +25,11 @@ def addCategory(checkin, category, checkins):
 
 def addCheckin(yelp, checkin, category, checkins):
     split_checkin = checkin.split("-")
-    if int(split_checkin[1]) < 10:
-        split_checkin[1] = '0' + split_checkin[1]
-    translate_checkin = daytonumber(split_checkin[0])+split_checkin[1]
+    # if int(split_checkin[1]) < 10:
+    hour = round(float(split_checkin[1]) / 24, 2)
+    translate_checkin = int((daytonumber(split_checkin[0])+hour) * 100)
+    # translate_checkin = int(daytonumber(split_checkin[0]) * 24) + int(split_checkin[1])
+    # translate_checkin = datetime(2017, 7, daytonumber(split_checkin[0]), int(split_checkin[1]))
 
     if translate_checkin not in yelp:
         yelp[translate_checkin] = {}
